8ad34bf198d6ddca1d10e1a56bf96a1caa96273b
Lucas Steer <lucassteer@gmail.com>
2024-04-14 16:01:15 -0400

    Restructure project to have frontend be top-level

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'photo-sharing-frontend' title`, () => {
+  it(`should have the 'photo-sharing-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('photo-sharing-frontend');
+    expect(app.title).toEqual('photo-sharing-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, photo-sharing-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, photo-sharing-app'
+    );
   });
 });

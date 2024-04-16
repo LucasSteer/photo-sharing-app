345ae9a184fe7f2600465e04137e58d3128bf1a8
Lucas Steer <lucassteer@gmail.com>
2024-04-15 22:03:47 -0400

    Add simple photo upload route

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 const photoRoute = express.Router();
 const uri = process.env.MongoDBConnectionString;
 const mongoose = require("mongoose");
+const { Readable } = require("stream");
+
+const multer = require("multer");
+var storage = multer.memoryStorage();
+var upload = multer({
+  storage: storage,
+  limits: { fields: 1, fileSize: 6000000, files: 1, parts: 2 },
+});
 
 let db;
 mongoose.connect(uri).then(() => {
@@ -36,6 +44,40 @@ photoRoute.get("/:photoID", (req, res) => {
 
   downloadStream.on("end", () => {
     res.end();
+  });
+});
+
+photoRoute.post("/", (req, res) => {
+  // TODO: permit only image files
+  upload.single("photo")(req, res, (err) => {
+    if (err) {
+      return res
+        .status(400)
+        .json({ message: "Upload Request Validation Failed" });
+    }
+
+    const readablePhotoStream = new Readable();
+    readablePhotoStream.push(req.file.buffer);
+    readablePhotoStream.push(null);
+
+    let bucket = new mongoose.mongo.GridFSBucket(db, {
+      bucketName: "photos",
+    });
+
+    let uploadStream = bucket.openUploadStream(`${Date.now()}-user-photo`);
+    readablePhotoStream.pipe(uploadStream);
+
+    uploadStream.on("error", () => {
+      return res.status(500).json({ message: "Error uploading file" });
+    });
+
+    uploadStream.on("finish", () => {
+      return res.status(201).json({
+        message:
+          "File uploaded successfully, stored under Mongo ObjectID: " +
+          uploadStream.id,
+      });
+    });
   });
 });
 

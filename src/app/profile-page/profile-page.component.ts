6501b1535cb16d0e8797bf960e99b34543390e4f
Lucas Steer <lucassteer@gmail.com>
2024-04-17 15:48:02 -0400

    Add Nav Bar

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FileUploaderComponent } from '../file-uploader/file-uploader.component';
 import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -12,13 +11,6 @@ import { AuthService } from '../auth.service';
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
-export class ProfilePageComponent implements OnInit {
-  userId = '';
-
-  constructor(private http: HttpClient, private auth: AuthService) {}
-
-  ngOnInit(): void {
-    this.auth.autoAuthUser();
-    this.userId = this.auth.getUserId();
-  }
+export class ProfilePageComponent {
+  constructor(public auth: AuthService) {}
 }

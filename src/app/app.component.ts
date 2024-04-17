24c1d66aa8acce60cc92b1c2727a35a4b83b06b2
Lucas Steer <lucassteer@gmail.com>
2024-04-16 20:51:42 -0400

    Add simple form for creating a user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FileUploaderComponent } from './file-uploader/file-uploader.component';
 import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FileUploaderComponent, PhotoGalleryComponent],
+  imports: [RouterOutlet, FileUploaderComponent, PhotoGalleryComponent, SignupFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-signup-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup-form.component.html',
+  styleUrl: './signup-form.component.css'
+})
+export class SignupFormComponent {
+  signupForm = new FormGroup({
+    email: new FormControl(''),
+  });
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.post(`/users`, this.signupForm.value).subscribe({next: (res: any) => {
+      console.log(res.message);
+    }, error: (err: any) => {
+      console.error("Error: ", err);
+    }});
+  }
+}

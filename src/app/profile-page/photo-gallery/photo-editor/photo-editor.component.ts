1d472c825f86839f9d3b854dfeac54fa16098451
Lucas Steer <lucassteer@gmail.com>
2024-04-17 18:26:37 -0400

    Simple styling for the photo editor

--- src/app/profile-page/photo-gallery/photo-editor/photo-editor.component.ts
+++ src/app/profile-page/photo-gallery/photo-editor/photo-editor.component.ts
@@ -3,19 +3,20 @@ import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { PhotoComponent } from '../../../photo/photo.component';
-import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-photo-editor',
   standalone: true,
-  imports: [MatIconModule, PhotoComponent],
+  imports: [MatIconModule, PhotoComponent, MatCardModule, MatButtonModule],
   templateUrl: './photo-editor.component.html',
   styleUrl: './photo-editor.component.css'
 })
 export class PhotoEditorComponent {
   @Input() photoName = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient) {}
 
   onClickDelete() {
     this.http.delete(`/photos/${this.photoName}`).subscribe({next: (res: any) => {
